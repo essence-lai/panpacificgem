1f2c979a4f8c5a9ab3f7ccccd3602dd4305e8a35
essence-lai <essence.alumni.ubc.ca>
2018-06-29 16:33:03 -0700

    products with example completed

--- src/actions/ProductsActions.js
+++ src/actions/ProductsActions.js
@@ -4,14 +4,12 @@ export const GET_PRODUCTS = 'get_products';
 export const PRODUCTS_STATUS = 'products_status';
 
 export function getProducts(e) {
-    console.log('itmade it');
     return dispatch => {
         dispatch({
             type: PRODUCTS_STATUS,
             payload: true
         });
         database.ref(e).on('value', db => {
-            console.log(db.val());
             dispatch({
                 type: GET_PRODUCTS,
                 payload: db.val()
--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button ,Navbar, NavDropdown, Nav, MenuItem, Collapse} from 'react-bootstrap';
+import { Jumbotron, Grid, Row, Col, Image, Button ,Navbar, NavDropdown, Nav, MenuItem, Collapse, Carousel, NavItem} from 'react-bootstrap';
 import './styles.css'
 import  MainNavbar from '../navBar/index';
 import { connect } from 'react-redux';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { getProducts} from "../../actions/ProductsActions";
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import ProductsImage from '../productsImage/index'
+
+library.add(faHome);
+
 
 class  Products extends React.Component {
     constructor(props){
@@ -12,22 +19,30 @@ class  Products extends React.Component {
         this.state = {
             comingOutSoon: { Ornaments: false, Miscellaneous: false},
             productsShow: false,
+            main: true
         };
         this.handleSoon = this.handleSoon.bind(this);
         this.handleProduct = this.handleProduct.bind(this);
+        this.handleHome = this.handleHome.bind(this);
     }
 
     handleSoon = (e) =>{
-        this.setState({comingOutSoon: {Bracelets: false, Brooches: false, Carvings: false, Earrings: false, Necklaces: false, Ornaments: false, Pendants: false, Rings: false, Miscellaneous: false} , productsShow: false});
+        this.setState({comingOutSoon: {Bracelets: false, Brooches: false, Carvings: false, Earrings: false, Necklaces: false, Ornaments: false, Pendants: false, Rings: false, Miscellaneous: false} , productsShow: false, main: false});
         this.setState({comingOutSoon: {[e]: true}})
 
     };
 
     handleProduct = (e) => {
-        this.setState({comingOutSoon: {Bracelets: false, Brooches: false, Carvings: false, Earrings: false, Necklaces: false, Ornaments: false, Pendants: false, Rings: false, Miscellaneous: false} , productsShow: false});
+        this.setState({comingOutSoon: {Bracelets: false, Brooches: false, Carvings: false, Earrings: false, Necklaces: false, Ornaments: false, Pendants: false, Rings: false, Miscellaneous: false} , productsShow: false, main: false});
         this.props.getProducts(e);
         this.setState({productsShow: true});
     };
+
+    handleHome = () => {
+        this.setState({comingOutSoon: {Bracelets: false, Brooches: false, Carvings: false, Earrings: false, Necklaces: false, Ornaments: false, Pendants: false, Rings: false, Miscellaneous: false} , productsShow: false, main: false});
+        this.setState({main: true});
+    };
+
     render() {
         const { products } = this.props;
         return (
@@ -38,7 +53,22 @@ class  Products extends React.Component {
                     </Grid>
                   <Grid className="product-handler">
                       <Grid>
-                          <Navbar inverse default>
+                          <Navbar inverse default  collapseOnSelect>
+                              <Navbar.Header>
+                                  <Navbar.Brand>
+                                      <NavItem
+                                          eventKey={10}
+                                          onClick={() => {
+                                              this.handleHome()
+                                          }}
+
+                                      >
+                                          <FontAwesomeIcon icon="home" color="white"/>
+                                      </NavItem>
+                                  </Navbar.Brand>
+                                  <Navbar.Toggle/>
+                              </Navbar.Header>
+                              <Navbar.Collapse>
                               <Nav className="nav-color">
                                   <NavDropdown
                                       eventkey={1}
@@ -247,9 +277,50 @@ class  Products extends React.Component {
                                       > Coming Out Soon!</MenuItem>
                                   </NavDropdown>
                               </Nav>
+                              </Navbar.Collapse>
                           </Navbar>
                       </Grid>
                       <Grid>
+                          {this.state.main &&
+                          <Grid className="product-grid">
+                              <Grid className="title-grid">
+                                    <Image className="product-banner-image" src="../../assets/products.jpeg" />
+                                    <h1 className="product-font">Products</h1>
+                              </Grid>
+                              <h1 className="product-visit-us-font">
+                                  Visit us at the following Gift Shows
+                              </h1>
+                              <Carousel className="product-carousel">
+                                  <Carousel.Item className="product-carousel-visit-us" >
+                                      <a target="_blank" rel="noopener noreferrer"  href='https://www.cangift.org/toronto-gift-fair/en/home/'>
+                                          <Image className="product-image-carousel" src="../../assets/toronto.jpg"/>
+                                          <Carousel.Caption >
+                                              <h2 className="product-carousel-two-font-main"> Toronto Gift Show</h2>
+                                              <h3 className="product-carousel-two-font-sub"> September 16 - 17</h3>
+                                          </Carousel.Caption>
+                                      </a>
+                                  </Carousel.Item>
+                                  <Carousel.Item className="product-carousel-visit-us">
+                                      <a target="_blank" rel="noopener noreferrer"  href="https://www.cangift.org/alberta-gift-fair/en/home/">
+                                          <Image className="product-image-carousel" src="../../assets/alberta.jpg"/>
+                                          <Carousel.Caption>
+                                              <h2  className="product-carousel-two-font-main"> Alberta Gift Show</h2>
+                                              <h3 className="product-carousel-two-font-sub"> August 26 -29</h3>
+                                          </Carousel.Caption>
+                                      </a>
+                                  </Carousel.Item>
+                                  <Carousel.Item className="product-carousel-visit-us">
+                                      <a target="_blank" rel="noopener noreferrer"  href="http://www.vancouvergiftexpo.com/">
+                                          <Image className="product-image-carousel" src="../../assets/vancouver.jpg"/>
+                                          <Carousel.Caption>
+                                              <h2 className="product-carousel-two-font-main"> Vancouver Gift Show</h2>
+                                              <h3 className="product-carousel-two-font-sub"> August 12 -14</h3>
+                                          </Carousel.Caption>
+                                      </a>
+                                  </Carousel.Item>
+                              </Carousel>
+                          </Grid>
+                          }
                           {
                               this.state.comingOutSoon.Bracelets &&
                               <Jumbotron className="coming-out-soon">
@@ -333,9 +404,13 @@ class  Products extends React.Component {
                           }
                           {
                               ( products.length >= 2 && this.state.productsShow ) &&
-                                  <Jumbotron className="coming-out-soon">
-                                      <h1>WOOHO</h1>
-                                  </Jumbotron>
+                                  <Grid className="center-grid">
+                                      <Grid className="title-grid">
+                                          <Image className="product-banner-image" src="../../assets/jadeCarvings.jpeg" />
+                                          <h1 className="product-font">Jade Carvings</h1>
+                                      </Grid>
+                                      <ProductsImage list={products}/>
+                                  </Grid>
                           }
                       </Grid>
                   </Grid>
